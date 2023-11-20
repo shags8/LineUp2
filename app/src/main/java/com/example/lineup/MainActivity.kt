38edee45e67710c2ad855f1fa380a4a009920567
Kanak Goel <goelk0308@gmail.com>
2023-11-20 23:36:44 +0530

    Location permission

--- app/src/main/java/com/example/lineup/MainActivity.kt
+++ app/src/main/java/com/example/lineup/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.lineup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv=findViewById<TextView>(R.id.tv)
+        tv.setOnClickListener {
+            val i= Intent(this,WelcomePage::class.java)
+            startActivity(i)
+        }
+
     }
 }
