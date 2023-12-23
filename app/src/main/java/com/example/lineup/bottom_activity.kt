2df474f85f9b90039442bc2aced327d581fa4fab
Kanak Goel <goelk0308@gmail.com>
2023-12-23 22:50:00 +0530

    login changes

--- app/src/main/java/com/example/lineup/bottom_activity.kt
+++ app/src/main/java/com/example/lineup/bottom_activity.kt
@@ -1,6 +1,9 @@
 package com.example.lineup
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.lineup.databinding.ActivityBottomBinding
@@ -36,6 +39,19 @@ class bottom_activity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout,fragment)
         fragmentTransaction.commit()
     }
-
-
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setTitle("Exit")
+            .setMessage("Are you sure?")
+            .setPositiveButton("yes", DialogInterface.OnClickListener { dialog, which ->
+                val intent = Intent(Intent.ACTION_MAIN)
+                intent.addCategory(Intent.CATEGORY_HOME)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            })
+            .setNegativeButton("no", DialogInterface.OnClickListener { dialog, which ->
+                super.onBackPressed()
+            })
+            .show()
+    }
     }
