7a1b3b43aefebe9c0335e0232cfcb1021c068562
Kanak Goel <goelk0308@gmail.com>
2024-01-03 23:49:41 +0530

    minor changes

--- app/src/main/java/com/example/lineup/Qr_code.kt
+++ app/src/main/java/com/example/lineup/Qr_code.kt
@@ -1,88 +1,66 @@
 package com.example.lineup
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.KeyEvent
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
 import com.google.zxing.ResultPoint
-import com.google.zxing.client.android.BeepManager
+import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.journeyapps.barcodescanner.DefaultDecoderFactory
-
-
+import com.journeyapps.barcodescanner.ViewfinderView
 
 class Qr_code : Fragment() {
 
-    private lateinit var barcodeView: DecoratedBarcodeView
-    private lateinit var beepManager: BeepManager
-    private var lastText: String? = null
+    private var database = FirebaseDatabase.getInstance()
+    val userid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+    private val userInfo=database.getReference("users/$userid")
+    private lateinit var zeal_id:String
 
-    private lateinit var scannedTextView: TextView
-
-    private val callback = object : BarcodeCallback {
-        override fun barcodeResult(result: BarcodeResult) {
-            if (result.text == null || result.text == lastText) {
-                // Prevent duplicate scans
-                return
-            }
-
-            lastText = result.text
-            barcodeView.setStatusText(result.text)
-
-            beepManager.playBeepSoundAndVibrate()
-
-
-
-        }
-
-        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
-    }
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_qr_code, container, false)
-
-        barcodeView = view.findViewById(R.id.barcode_scanner)
-        val formats = listOf(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
-        barcodeView.barcodeView.decoderFactory = DefaultDecoderFactory(formats)
-        barcodeView.initializeFromIntent(requireActivity().intent)
-        barcodeView.decodeContinuous(callback)
-
-        beepManager = BeepManager(requireActivity())
-
         return view
     }
-    override fun onResume() {
-        super.onResume()
-        barcodeView.resume()
-    }
 
-    override fun onPause() {
-        super.onPause()
-        barcodeView.pause()
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val qr=view.findViewById<ImageView>(R.id.qr_code)
+        userInfo.get().addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val snapshot=task.result
+                zeal_id=snapshot.child("zealid").value.toString()
+                Log.e("id123", zeal_id)
 
-    fun pause() {
-        barcodeView.pause()
-    }
+            }else{
+                Log.e("TAG","Error getting data")
+            }
 
-    fun resume() {
-        barcodeView.resume()
-    }
+        }
+        //   Log.e("id123","$zeal_id")
+        // btn.setOnClickListener {
+        val multiFormatWriter = MultiFormatWriter()
+        try {
+            val multiFormatWriter = MultiFormatWriter()
+            val bitMatrix: BitMatrix = multiFormatWriter.encode(userid, BarcodeFormat.QR_CODE, 300, 300)
+            val barcodeEncoder = BarcodeEncoder()
+            val bitMap: Bitmap = barcodeEncoder.createBitmap(bitMatrix)
 
-    fun triggerScan() {
-        barcodeView.decodeSingle(callback)
-    }
+            qr.setImageBitmap(bitMap)
 
+        } catch (e: Exception) {
+            // Toast.makeText(this, "Unable to generate QR", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
