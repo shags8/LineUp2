812c8294fd936e300a2f4a7b80a1799c2d2a2eba
Shagun <shagunchahar8@gmail.com>
2024-01-06 19:48:51 +0530

    characterselect

---
+++ app/src/main/java/com/example/lineup/AvatarAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.lineup
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lineup.databinding.CharacterLayoutBinding
+
+class AvatarAdapter( private val images: IntArray) :
+    RecyclerView.Adapter<AvatarAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CharacterLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        Log.d("AvatarAdapter4", "Inflating CharacterLayoutBinding")
+        Log.d("AvatarAdapter6", "{$itemCount}")
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageId = images[position]
+        Log.d("AvatarAdapter5", "{$imageId}")
+        holder.binding.avatarImg.setImageResource(imageId)  // Assuming your layout has an ImageView with ID "avatarImg"
+    }
+
+    override fun getItemCount(): Int = images.size
+
+    class ViewHolder(val binding: CharacterLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+}
