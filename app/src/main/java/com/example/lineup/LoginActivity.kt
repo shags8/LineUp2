82558a3b331172583dc6726842db30d6cde886af
Kanak Goel <goelk0308@gmail.com>
2024-01-06 18:45:19 +0530

    login and signup

--- app/src/main/java/com/example/lineup/LoginActivity.kt
+++ app/src/main/java/com/example/lineup/LoginActivity.kt
@@ -10,12 +10,17 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.startActivity
+import com.example.lineup.dataClass.Login
+import com.example.lineup.dataClass.SignUp
 import com.example.lineup.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 public class LoginActivity : AppCompatActivity() {
@@ -34,40 +39,61 @@ public class LoginActivity : AppCompatActivity() {
 
 val loginbtn = binding.loginBtn
      //   Log.e("zealID","$zeal")
-        userInfo.get().addOnCompleteListener { task ->
-            if(task.isSuccessful){
-                val snapshot=task.result
-                zeal_id=snapshot.child("zealid").value.toString()
-                auth_password=snapshot.child("Password").value.toString()
-
+//        userInfo.get().addOnCompleteListener { task ->
+//            if(task.isSuccessful){
+//                val snapshot=task.result
+//                zeal_id=snapshot.child("zealid").value.toString()
+//                auth_password=snapshot.child("Password").value.toString()
+//
                 loginbtn.setOnClickListener {
                     val zeal = binding.zeal.text.toString()
                     val password = binding.password.text.toString()
 
                     if(zeal.isEmpty() || password.isEmpty())
                     {
-                        //Log.e("zealID","$zeal")
                         Toast.makeText(this, "Please enter your ZealId and Password", Toast.LENGTH_SHORT).show()
                     }
-                    else{
+                    else {
+                        val userLogin = Login(password, zeal)
+                        val call = RetrofitApi.apiInterface.login(userLogin)
+                        call.enqueue(object : Callback<Login> {
+                            override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "Login Successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                val intent = Intent(this@LoginActivity, bottom_activity::class.java)
+                                startActivity(intent)
+                            }
 
-                        if(zeal == zeal_id && password == auth_password){
-                            Toast.makeText(this,"User verified",Toast.LENGTH_SHORT).show()
-                            startActivity(Intent(this,bottom_activity::class.java))
-                            finish()
-                        }else{
-                            Toast.makeText(this,"Oops! User not registered ",Toast.LENGTH_SHORT).show()
-                        }
+                            override fun onFailure(call: Call<Login>, t: Throwable) {
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    t.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                            }
+                        })
                     }
-                }
+
+
+//                        if(zeal == zeal_id && password == auth_password){
+//                            Toast.makeText(this,"User verified",Toast.LENGTH_SHORT).show()
+//                            startActivity(Intent(this,bottom_activity::class.java))
+//                            finish()
+//                        }else{
+//                            Toast.makeText(this,"Oops! User not registered ",Toast.LENGTH_SHORT).show()
+//                        }
+
+
 
              //   Log.e("id123", zeal_id)
 
-            }else{
-              //  Log.e("TAG","Error getting data")
             }
 
         }
-    }
+
 
 }
---
+++ app/src/main/java/com/example/lineup/dataClass/Login.kt
@@ -0,0 +1,6 @@
+package com.example.lineup.dataClass
+
+data class Login(
+    val password:String,
+    val zealId:String
+)
